f7f5bda34978bba89560784b1bdbd85f277449ac
Kamal Maged <kamal_maged@yahoo.com>
2019-06-18 21:40:46 -0400

    Initial Commit

---
+++ EnteringAHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnteringAHouse
+//
+//  Created by Kamal Maged on 2019-06-18.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
